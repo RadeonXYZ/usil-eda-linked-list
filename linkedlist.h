847db59089cd7eb6c8587bab2b2537c91892e0ee
RadeonXYZ <terronesjose1999@gmail.com>
2021-04-14 09:27:05 -0500

    Revert "Examen"
    This reverts commit 7c8bd1a58541926d693d93ae4053db7d27da67e8.

--- linkedlist.h
+++ linkedlist.h
@@ -6,35 +6,20 @@
 // using namespace std;
 
 template <typename T>
-//by jose terrones
-class Node
-{
-  private:
-    //Declarar punteros
-    T Data
-    Node *next;
-    Node *prev;
-  public:
-}
-
-template <typename T>
 class LinkedList
 {
   private:
-  // TM tamaño maximo, TA tamaño actual
-    T TA; 
-    T TM;
   // members here
   public:
     T front(); // Retorna el elemento al comienzo
     T back(); // Retorna el elemento al final
-    void push_front(T); // Agrega un elemento al comienzo 
-    void push_back(T); // Agrega un elemento al final
-    void pop_front(); // Remueve el elemento al comienzo
-    void pop_back(); // Remueve el elemento al final
-    T operator[](int); // Retorna el elemento en la posición indicada
+    void push_front(T &elem); // Agrega un elemento al comienzo 
+    void push_back(T &elem); // Agrega un elemento al final
+    void pop_front(); // Remueve el elemento al comienzo pero no lo retorna
+    void pop_back(); // Remueve el elemento al final pero no lo retorna
+    T operator[](size_t pos); // Retorna el elemento en la posición indicada
     bool empty(); // Retorna si la lista está vacía o no
-    int size(); // Retorna el tamaño de la lista 
+    size_t size(); // Retorna el tamaño de la lista 
     void clear(); // Elimina todos los elementos de la lista
     void sort(); // Ordena la lista
     void reverse(); // Revierte la lista
@@ -46,23 +31,6 @@ void LinkedList<T>::front(T &elem)
 {
 
 }
-template <typename T>
-void LinkedList<T>::push_front(T);
-
-// 
-template <typename T>
-//by jose terrones
-bool LinkedList<T>::empty()
-  {
-    //Si el TA es cero da verdadero
-    return (TA == 0);
-  }
-template <typename T>
-T LinkedList<T>::size()
-  {
-    //retorna valor maximo
-    return TM;
-  }
 
 // Comments #3
 template <typename T>
